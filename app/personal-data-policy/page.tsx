d3b8b03d18c3453f55d57c478884fc367be0386c
pechenka366 <114060538+pechenka366@users.noreply.github.com>
2024-06-19 19:54:24 +0900

    Add files via upload

---
+++ app/personal-data-policy/page.tsx
@@ -0,0 +1,6 @@
+import PersonalDataPolicyPage from "@/components/templates/PersonalDataPolicyPage/PersonalDataPolicyPage";
+
+
+export default function PersonalDataPolicy() {
+  return <PersonalDataPolicyPage />
+}
